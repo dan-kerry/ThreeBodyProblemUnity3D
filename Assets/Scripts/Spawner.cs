0612eb19920ac11a7b113623e66a2bd5d3c5c982
Daniel Kerry <psxdk7@exmail.nottingham.ac.uk>
2022-05-21 12:41:43 +0100

    Pre-Bodt rework

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,22 +8,39 @@ public class Spawner : MonoBehaviour
 
     public int BodyCount;
     public GameObject BodyPrefab;
+    
+
+
+    private static BodyConfig B1 = new BodyConfig(new Vector3(-0.97000436f, 0.24308753f, 0f), new Vector3(0.466203685f, 0.43236573f, 0f),new Vector3(1f, 1f, 1f));
+    private static BodyConfig B2 = new BodyConfig(new Vector3(0.97000436f, -0.24308753f, 0f), new Vector3(0.466203685f, 0.43236573f, 0f),new Vector3(1f, 1f, 1f));
+    private static BodyConfig B3 = new BodyConfig(new Vector3(0f, 0f, 0f), new Vector3(-0.93240737f, -0.86473146f, 0f),new Vector3(1f, 1f, 1f));
+    private BodyConfig[] Figure8 = new BodyConfig[3] {B1, B2, B3};
+ 
 
     
     
+void SetStart() {
+    foreach (BodyConfig config in Figure8) {
+        GameObject newObject = Instantiate(BodyPrefab, config.Position, Quaternion.identity);
 
+    }
    
     // Start is called before the first frame update
     void Start()
-    
     {
-        for (int i = 0; i < BodyCount; i++)
-        {
-            //Vector3 randomLocation = new Vector3 (Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f));
-            GameObject newObject = Instantiate(BodyPrefab, Vector3.zero, Quaternion.identity);
-            
+        //RandomStart();
+        SetStart();
     }
 
+void RandomStart() {
+    for (int i = 0; i < BodyCount; i++) {
+    GameObject newObject = Instantiate(BodyPrefab, Vector3.zero, Quaternion.identity);
+    }
+
+
+    
+
+}
 
 }
 
