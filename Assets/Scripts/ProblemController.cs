5a2f74f64b00ac17315edc31bb8a62dbecf9074b
Daniel Kerry <psxdk7@exmail.nottingham.ac.uk>
2022-05-21 10:24:18 +0100

    Local Grav Calculations

--- Assets/Scripts/ProblemController.cs
+++ Assets/Scripts/ProblemController.cs
@@ -27,73 +27,7 @@ public class ProblemController : MonoBehaviour
          }
     }
 
-    Vector3[] Solver(GameObject[] BodyArray) {
-        //Todo: This needs to be scaled so that it can work on n > 2. For 2 bodies only it may require a seperate solver solution.
-    
-
-        int ArrayLen = BodyArray.Length;
-    
-        outputVectors[0] = -9.8f * BodyArray[1].transform.localScale[0] * (BodyArray[0].transform.localPosition - BodyArray[1].transform.localPosition) / 
-                MathF.Pow((MathF.Sqrt(
-                MathF.Pow(BodyArray[0].transform.localPosition[0] - BodyArray[1].transform.localPosition[0] ,2.0f) + 
-                MathF.Pow(BodyArray[0].transform.localPosition[1] - BodyArray[1].transform.localPosition[1] ,2.0f) +
-                MathF.Pow(BodyArray[0].transform.localPosition[2] - BodyArray[1].transform.localPosition[2] ,2.0f))), 3.0f) -
-
-                9.8f * BodyArray[2].transform.localScale[0] * (BodyArray[0].transform.localPosition - BodyArray[2].transform.localPosition) / 
-                MathF.Pow((MathF.Sqrt(
-                MathF.Pow(BodyArray[0].transform.localPosition[0] - BodyArray[2].transform.localPosition[0] ,2.0f) + 
-                MathF.Pow(BodyArray[0].transform.localPosition[1] - BodyArray[2].transform.localPosition[1] ,2.0f) +
-                MathF.Pow(BodyArray[0].transform.localPosition[2] - BodyArray[2].transform.localPosition[2] ,2.0f))), 3.0f)
-                ;
-            
-            Rigidbody rb = BodyArray[0].GetComponent<Rigidbody>();
-            rb.AddForce(outputVectors[0], ForceMode.Acceleration);
-            
-        outputVectors[1] = -9.8f * BodyArray[2].transform.localScale[0] * (BodyArray[1].transform.localPosition - BodyArray[2].transform.localPosition) / 
-                MathF.Pow((MathF.Sqrt(
-                MathF.Pow(BodyArray[1].transform.localPosition[0] - BodyArray[2].transform.localPosition[0] ,2.0f) + 
-                MathF.Pow(BodyArray[1].transform.localPosition[1] - BodyArray[2].transform.localPosition[1] ,2.0f) +
-                MathF.Pow(BodyArray[1].transform.localPosition[2] - BodyArray[2].transform.localPosition[2] ,2.0f))), 3.0f) -
-
-                9.8f * BodyArray[0].transform.localScale[0] * (BodyArray[1].transform.localPosition - BodyArray[0].transform.localPosition) / 
-                MathF.Pow((MathF.Sqrt(
-                MathF.Pow(BodyArray[1].transform.localPosition[0] - BodyArray[0].transform.localPosition[0] ,2.0f) + 
-                MathF.Pow(BodyArray[1].transform.localPosition[1] - BodyArray[0].transform.localPosition[1] ,2.0f) +
-                MathF.Pow(BodyArray[1].transform.localPosition[2] - BodyArray[0].transform.localPosition[2] ,2.0f))), 3.0f)
-                ;
-
-                Rigidbody rb1 = BodyArray[1].GetComponent<Rigidbody>();
-                rb1.AddForce(outputVectors[1], ForceMode.Acceleration);
-        
-        outputVectors[2] = -9.8f * BodyArray[0].transform.localScale[0] * (BodyArray[2].transform.localPosition - BodyArray[0].transform.localPosition) / 
-                MathF.Pow((MathF.Sqrt(
-                MathF.Pow(BodyArray[2].transform.localPosition[0] - BodyArray[0].transform.localPosition[0] ,2.0f) + 
-                MathF.Pow(BodyArray[2].transform.localPosition[1] - BodyArray[0].transform.localPosition[1] ,2.0f) +
-                MathF.Pow(BodyArray[2].transform.localPosition[2] - BodyArray[0].transform.localPosition[2] ,2.0f))), 3.0f) -
-
-                9.8f * BodyArray[1].transform.localScale[0] * (BodyArray[2].transform.localPosition - BodyArray[1].transform.localPosition) / 
-                MathF.Pow((MathF.Sqrt(
-                MathF.Pow(BodyArray[2].transform.localPosition[0] - BodyArray[1].transform.localPosition[0] ,2.0f) + 
-                MathF.Pow(BodyArray[2].transform.localPosition[1] - BodyArray[1].transform.localPosition[1] ,2.0f) +
-                MathF.Pow(BodyArray[2].transform.localPosition[2] - BodyArray[1].transform.localPosition[2] ,2.0f))), 3.0f)
-                ;
-
-                Rigidbody rb2 = BodyArray[2].GetComponent<Rigidbody>();
-                rb2.AddForce(outputVectors[2], ForceMode.Acceleration);
-        
-        for (int i = 0; i > BodyArray.Length; i++) {
-            
-            //List<int> tempListA = BodyArray.ToList();
-           
-
-          
-            
-
-
-        }
-        
-        return outputVectors;
-    }
+  
 
 
     void BodyMover(Vector3[] dvs){
@@ -108,8 +42,6 @@ public class ProblemController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Bodies.Length > 0) {
-        Solver(Bodies);
-        }
+       
     }
 }
--- Assets/Scripts/nBodyCalculation.cs
+++ Assets/Scripts/nBodyCalculation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,13 @@ public class nBodyCalculation : MonoBehaviour
     public GameObject RegisterA;
     private List <GameObject> RegisterComplete;
     private List <GameObject> otherBodies;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         CreateBodyList();
-        ValidateBodyList();
+        rb = GetComponent<Rigidbody>();
+        //ValidateBodyList();
     }
     
     void CreateBodyList() {
@@ -30,9 +33,31 @@ public class nBodyCalculation : MonoBehaviour
         }
     }
 
+    Vector3 solveMotion(){
+        Vector3 result = Vector3.zero;
+        for (int i = 0; i < otherBodies.Count; i++) {
+            //Debug.Log(otherBodies[i].transform.localScale[0]);
+            result = result + -9.8f * otherBodies[i].transform.localScale[0] * (gameObject.transform.localPosition - otherBodies[i].transform.localPosition) / 
+                MathF.Pow((MathF.Sqrt(
+                MathF.Pow(gameObject.transform.localPosition[0] - otherBodies[i].transform.localPosition[0] ,2.0f) + 
+                MathF.Pow(gameObject.transform.localPosition[1] - otherBodies[i].transform.localPosition[1] ,2.0f) +
+                MathF.Pow(gameObject.transform.localPosition[2] - otherBodies[i].transform.localPosition[2] ,2.0f))), 3.0f);
+        }
+        return result;
+        //Debug.Log("SB: " +result);
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        Vector3 DeltaV = solveMotion();
+        rb.AddForce(DeltaV, ForceMode.Acceleration);
+    }
+
+    void OnDrawGizmos()
+    {
+        // Draw a yellow sphere at the transform's position
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, (transform.position+solveMotion()*2));
     }
 }
