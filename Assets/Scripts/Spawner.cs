4d1c8ee18c9651c6e85f368d9e83458bb4a2342a
Daniel Kerry <psxdk7@exmail.nottingham.ac.uk>
2022-06-10 16:51:11 +0100

    Revert "Working Xml Imports"
    This reverts commit 6118d26a0030e85b2fa3068da1cbc7ca394bba6b.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -3,21 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using static BodyConfig;
 
-
 public class Spawner : MonoBehaviour
 {
 
     public int BodyCount;
-    public  List <BodyData> Initials;
     public GameObject BodyPrefab;
-    Rigidbody rb;
-    Rigidbody rb2;
-    Rigidbody rb3;
-    public List <BodyData> InitialBodyList;
+     Rigidbody rb;
+     Rigidbody rb2;
+     Rigidbody rb3;
     
+/*
+    public Vector3[,] Figure8 = new [,] {{ new Vector3(-0.97000436f, 0.24308753f, 0f), new Vector3(0.466203685f, 0.43236573f, 0f),new Vector3(1f, 1f, 1f) },
+                                        {new Vector3(0.97000436f, -0.24308753f, 0f), new Vector3(0.466203685f, 0.43236573f, 0f),new Vector3(1f, 1f, 1f)},
+                                        {new Vector3(0f, 0f, 0f), new Vector3(-0.93240737f, -0.86473146f, 0f),new Vector3(1f, 1f, 1f)}} ;
 
+
+   
+    */
 void SetStart() {
-            /*
             GameObject newObject1 = Instantiate(BodyPrefab, Vector3.zero, Quaternion.identity);
             Vector3 randomLocation = new Vector3 (-0.97000436f, 0.24308753f, 0f);
             Vector3 randomForce = new Vector3 (0.466203685f, 0.43236573f, 0f);
@@ -47,7 +50,6 @@ void SetStart() {
             rb3 = newObject3.GetComponent<Rigidbody>();
             rb3.mass = BodyScale3;
             rb3.velocity = randomForce3;
-            */
 
 }
      
@@ -56,28 +58,15 @@ void SetStart() {
     // Start is called before the first frame update
     void Start()
     {
-        InitialBodyList = GetComponent<XmlParse>().InitialBodyList;
-        foreach (BodyData body in InitialBodyList)
-        {
-            Rigidbody rb;
-            GameObject newObject = Instantiate(BodyPrefab, Vector3.zero, Quaternion.identity);
-            Vector3 randomLocation = new Vector3 (body.XPosition, body.YPosition, body.ZPosition);
-            Vector3 randomForce = new Vector3 (body.XVelocity, body.YVelocity, body.ZVelocity);
-            newObject.transform.localScale = new Vector3(body.Mass, body.Mass, body.Mass);
-            newObject.transform.localPosition = randomLocation;
-            rb = newObject.GetComponent<Rigidbody>();
-            rb.mass = body.Mass;
-            rb.velocity = randomForce;
-        }
-
+        
+        //RandomStart();
         SetStart();
     }
 
-void RandomStart() 
-{
+void RandomStart() {
     for (int i = 0; i < BodyCount; i++) {
     GameObject newObject = Instantiate(BodyPrefab, Vector3.zero, Quaternion.identity);
-}
+    }
 
     
 
--- Assets/Scripts/XmlParse.cs
+++ Assets/Scripts/XmlParse.cs
@@ -13,15 +13,15 @@ public class XmlParse : MonoBehaviour
 {
     XDocument xmlDoc;
     IEnumerable<XElement> items; 
-    public List <BodyData> InitialBodyList = new List <BodyData>(); 
+    List <BodyData> data = new List <BodyData>(); 
 
     void Start() 
     {
         LoadXML();
-        StartCoroutine("AssignData");
-        //return InitialBodyList;
-    }
+        StartCoroutine ("AssignData");
+        Debug.Log(data[0].Mass);
 
+    }
     void LoadXML()
     {
         xmlDoc = XDocument.Load("/Users/dankerry/Documents/Projects/GPS/GPS/Assets/Scripts/Scenarios/Figure8.xml"); 
@@ -40,11 +40,14 @@ public class XmlParse : MonoBehaviour
             float YVel = (float)Convert.ToDouble(el.Element("InitialYVelocity").Value);
             float ZVel = (float)Convert.ToDouble(el.Element("InitialZVelocity").Value);
 
-            InitialBodyList.Add(new BodyData(XPos, YPos, ZPos, Mas, XVel, YVel, ZVel));                    
+            data.Add(new BodyData(XPos, YPos, ZPos, Mas, XVel, YVel, ZVel));                    
         }
         yield return null;
     }
 }
+
+
+
 
 public class BodyData
 {
@@ -59,5 +62,6 @@ public class BodyData
         XVelocity = InitialXVelocity;
         YVelocity = InitialYVelocity;
         ZVelocity = InitialZVelocity;
+        
     }
 }
